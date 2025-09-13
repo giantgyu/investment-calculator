1065f27545f042615eef73f2a0a320ad7077190a
giantgyu <jrmhfrancisco@gmail.com>
2025-09-13 15:55:30 +0800

    moved function to component

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -9,4 +9,33 @@ import { Component } from "@angular/core";
 })
 export class InvestmentResultsComponent {
   isCalculatePressed = true;
+  initialInvestment = 0;
+  duration = 0;
+  expectedReturn = 0;
+  annualInvestment = 0;
+
+  calculateInvestmentResults() {
+    const annualData = [];
+    let investmentValue = this.initialInvestment;
+
+    for (let i = 0; i < this.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear =
+        investmentValue * (this.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + this.annualInvestment;
+      const totalInterest =
+        investmentValue - this.annualInvestment * year - this.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: this.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested:
+          this.initialInvestment + this.annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
 }
